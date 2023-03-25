4e13eb593cfd940d3a92384d71ce31fa0649fb19
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-25 19:34:52 +0530

    Completed with the structure of the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { css } from "@emotion/react";
+import { makeStyles } from "@mui/styles";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Coinpage from "./pages/Coinpage";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
-    <div className="">
-      <p>Hello World</p>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" Component={Homepage} exact />
+          <Route path="/coins/:id" Component={Coinpage} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
