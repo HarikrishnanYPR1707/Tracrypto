758e7ccdd6e382b5c370c8fffa0853ffce8e416e
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-26 22:08:17 +0530

    Completed with creating a context file for USD & INR currency

--- src/CryptoContext.jsx
+++ src/CryptoContext.jsx
@@ -1,9 +1,21 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const Crypto = createContext();
 
 const CryptoContext = ({ children }) => {
-  return <Crypto.Provider>{children}</Crypto.Provider>;
+  const [currency, setCurrency] = useState("INR");
+  const [symbol, setSymbol] = useState("₹");
+
+  useEffect(() => {
+    if (currency === "INR") setSymbol("₹");
+    else if (currency === "USD") setSymbol("$");
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
 };
 
 export default CryptoContext;
