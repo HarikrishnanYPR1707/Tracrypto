c95b03fe52e3f15e3ed3c0694b9ec46877ee162d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-26 22:22:34 +0530

    imported crytoState for currency context

---
+++ src/components/Header.jsx
@@ -0,0 +1,65 @@
+import {
+  AppBar,
+  createTheme,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const Header = () => {
+  const history = useNavigate();
+
+  const { currency, setCurrency } = CryptoState();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => {
+                history("/");
+              }}
+              sx={{
+                flex: 1,
+                color: "gold",
+                fontFamily: "Montserrat",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+              variant="h5"
+            >
+              Tracrypto
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
