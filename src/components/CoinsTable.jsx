ab8776a5233325c5f24ce1946a2c82a2c39e3384
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-05 02:18:23 +0530

    Added fetchCoins function to fetch the list of coins

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import CoinList from "../config/api";
 
 const CoinsTable = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async () => {
+    const { data } = await axios.get(CoinList);
+  };
+
   return <div>Coins Table</div>;
 };
 
