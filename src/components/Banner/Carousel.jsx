80ae0c24d48c570ba05bff8a526c80544ae86b00
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-04 21:20:54 +0530

    Added a function for fetching trending coins

--- src/components/Banner/Carousel.jsx
+++ src/components/Banner/Carousel.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
 import React from "react";
+import { TrendingCoins } from "../../config/api";
 
 const Carousel = () => {
-  return <div>Carousel Here</div>;
+  const { currency, setCurrency } = CryptoState();
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
+  };
+
+  return (
+    <div
+      style={{
+        height: "50%",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      Carousel Here
+    </div>
+  );
 };
 
 export default Carousel;
