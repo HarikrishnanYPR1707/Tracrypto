a4716fbdea344ad16e35c5e033bc2cc679053c5d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-05 02:26:50 +0530

    Added comments

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,15 +1,23 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import CryptoContext from "../CryptoContext";
 import CoinList from "../config/api";
 
 const CoinsTable = () => {
+  // State for coins
   const [coins, setCoins] = useState([]);
+
+  // State for loading
   const [loading, setLoading] = useState(false);
 
+  // Destructuring the currency from CryptoContext
+  const { currency } = CryptoContext();
+
+  // Creating a functoin for fetching the list of coins
   const fetchCoins = async () => {
     setLoading(true);
 
-    const { data } = await axios.get(CoinList);
+    const { data } = await axios.get(CoinList(currency));
 
     setCoins(data);
     setLoading(false);
