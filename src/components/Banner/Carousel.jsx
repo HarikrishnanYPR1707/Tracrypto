6edf9b7ac01b6d10e34adb79ad487fde5a1242c6
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-12 22:10:04 +0530

    Modified Carousel.jsx

--- src/components/Banner/Carousel.jsx
+++ src/components/Banner/Carousel.jsx
@@ -6,109 +6,110 @@ import { CryptoState } from "../../CryptoContext";
 import { TrendingCoins } from "../../config/api";
 
 export function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 const Carousel = () => {
-  // State for managing trending coins
-  const [trending, setTrending] = useState([]);
+	// State for managing trending coins
+	const [trending, setTrending] = useState([]);
 
-  // Taking the context of the the currency
-  const { currency, symbol } = CryptoState();
+	// Taking the context of the the currency
+	const { currency, symbol } = CryptoState();
 
-  // Function for fetching trending coins
-  const fetchTrendingCoins = async () => {
-    const { data } = await axios.get(TrendingCoins(currency));
+	// Function for fetching trending coins
+	const fetchTrendingCoins = async () => {
+		const { data } = await axios.get(TrendingCoins(currency));
 
-    // Setting the trending state to data
-    setTrending(data);
-  };
+		// Setting the trending state to data
+		setTrending(data);
+	};
 
-  console.log(trending);
+	console.log(trending);
 
-  // Calling the function when this component is rendered
-  useEffect(() => {
-    fetchTrendingCoins();
-  }, [currency]);
+	// Calling the function when this component is rendered
+	useEffect(() => {
+		fetchTrendingCoins();
+	}, [currency]);
 
-  // Mapping trending data to items
-  const items = trending.map((coin) => {
-    let profit = coin.price_change_percentage_24h >= 0;
+	// Mapping trending data to items
+	const items = trending.map((coin) => {
+		let profit = coin.price_change_percentage_24h >= 0;
 
-    return (
-      // Adding link to go to different page
-      <Link
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          cursor: "pointer",
-          textTransform: "uppercase",
-          color: "white",
-        }}
-        to={`/coins/${coin.id}`}
-      >
-        {/* adding image of the fetched coin */}
-        <img
-          src={coin?.image}
-          alt={coin.name}
-          height="80"
-          style={{
-            marginBottom: 10,
-          }}
-        />
-        <span>
-          {coin?.symbol}
-          &nbsp;
-          <span
-            style={{
-              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
-              fontWeight: 500,
-            }}
-          >
-            {profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2)}%
-          </span>
-        </span>
-        <span style={{ fontSize: 22, fontWeight: 500 }}>
-          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
-        </span>
-      </Link>
-    );
-  });
+		return (
+			// Adding link to go to different page
+			<Link
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					cursor: "pointer",
+					textTransform: "uppercase",
+					color: "white",
+				}}
+				to={`/coins/${coin.id}`}
+			>
+				{/* adding image of the fetched coin */}
+				<img
+					src={coin?.image}
+					alt={coin.name}
+					height="80"
+					style={{
+						marginBottom: 10,
+					}}
+				/>
+				<span>
+					{coin?.symbol}
+					&nbsp;
+					<span
+						style={{
+							color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+							fontWeight: 500,
+						}}
+					>
+						{profit && "+"}{" "}
+						{coin?.price_change_percentage_24h?.toFixed(2)}%
+					</span>
+				</span>
+				<span style={{ fontSize: 22, fontWeight: 500 }}>
+					{symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+				</span>
+			</Link>
+		);
+	});
 
-  const responsive = {
-    0: {
-      items: 2,
-    },
-    512: {
-      items: 3,
-    },
-    1012: {
-      items: 5,
-    },
-  };
+	const responsive = {
+		0: {
+			items: 2,
+		},
+		512: {
+			items: 3,
+		},
+		1012: {
+			items: 5,
+		},
+	};
 
-  return (
-    <div
-      style={{
-        height: "50%",
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <AliceCarousel
-        mouseTracking
-        infinite
-        autoPlayInterval={1000}
-        animationDuration={1500}
-        disableDotsControls
-        disableButtonsControls
-        responsive={responsive}
-        autoPlay
-        items={items}
-      />
-    </div>
-  );
+	return (
+		<div
+			style={{
+				height: "50%",
+				display: "flex",
+				alignItems: "center",
+			}}
+		>
+			<AliceCarousel
+				mouseTracking
+				infinite
+				autoPlayInterval={1000}
+				animationDuration={1500}
+				disableDotsControls
+				disableButtonsControls
+				responsive={responsive}
+				autoPlay
+				items={items}
+			/>
+		</div>
+	);
 };
 
 export default Carousel;
