047718a2430cbc27286eeb7103bc779b8f0491aa
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-12 22:10:45 +0530

    Modified Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,12 +3,12 @@ import Banner from "../components/Banner/Banner";
 import CoinsTable from "../components/CoinsTable";
 
 const HomePage = () => {
-  return (
-    <>
-      <Banner />
-      <CoinsTable />
-    </>
-  );
+	return (
+		<>
+			<Banner />
+			<CoinsTable />
+		</>
+	);
 };
 
 export default HomePage;
