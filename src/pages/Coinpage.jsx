df05c603e8a102af26e1bb1cb5c789fbdb9fc806
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-13 02:23:12 +0530

    created some variables for state management and for url and created fetch function for coins

--- src/pages/Coinpage.jsx
+++ src/pages/Coinpage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+import { SingleCoin } from "../config/api";
 
 const CoinPage = () => {
-  return <div>Coin Page</div>;
+	// Getting the id from the url
+	const { id } = useParams;
+
+	// creating state for coin
+	const [coin, setCoin] = useState();
+
+	// bringing ' currency ' and ' symbol ' from cryptoState context
+	const { currency, symbol } = CryptoState();
+
+	// fetching the particular coin
+	const fetchCoin = async () => {
+		const { data } = await axios.get(SingleCoin(id));
+
+		setCoin(data);
+	};
+
+	// calling the fetchCoin function
+	useEffect(() => {
+		fetchCoin();
+	}, []);
+
+	return <div>Coin Page</div>;
 };
 
 export default CoinPage;
