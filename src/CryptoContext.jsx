251a071aa45d071f8bf6429bbd0ce13ee1439197
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-04-12 22:09:39 +0530

    Modified CryptoContext.jsx

--- src/CryptoContext.jsx
+++ src/CryptoContext.jsx
@@ -3,23 +3,23 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const Crypto = createContext();
 
 const CryptoContext = ({ children }) => {
-  const [currency, setCurrency] = useState("INR");
-  const [symbol, setSymbol] = useState("₹");
+	const [currency, setCurrency] = useState("INR");
+	const [symbol, setSymbol] = useState("₹");
 
-  useEffect(() => {
-    if (currency === "INR") setSymbol("₹");
-    else if (currency === "USD") setSymbol("$");
-  }, [currency]);
+	useEffect(() => {
+		if (currency === "INR") setSymbol("₹");
+		else if (currency === "USD") setSymbol("$");
+	}, [currency]);
 
-  return (
-    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
-      {children}
-    </Crypto.Provider>
-  );
+	return (
+		<Crypto.Provider value={{ currency, symbol, setCurrency }}>
+			{children}
+		</Crypto.Provider>
+	);
 };
 
 export default CryptoContext;
 
 export const CryptoState = () => {
-  return useContext(Crypto);
+	return useContext(Crypto);
 };
